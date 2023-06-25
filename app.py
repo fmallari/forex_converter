7173c15fa687578b9bc0c53de06e08e20d5fd718
fmallari <fc.mallari@gmail.com>
2023-06-25 13:53:20 -0700

    update form and input

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, requests, render_template
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+import requests 
 
 app = Flask(__name__)
-app.config['SECRET KEY'] = "S3CRET"
+app.config['SECRET_KEY'] = "S3CRET"
+
+debug = DebugToolbarExtension(app)
 
 url = 'https://api.exchangerate.host/latest'
 response = requests.get(url)
@@ -10,7 +15,19 @@ data = response.json()
 print(data)
 
 @app.route("/")
-def show_forex_form():
+def homepage_form():
     """display converter form"""
 
-    return render_template("index.html", )
+    return render_template("form.html")
+
+@app.route("/result", methods=["POST"])
+def convert_currency():
+    """Convert currency after selecting Convert button"""
+    currency1 = request.args("from")
+    print(currency1);
+    currency2 = request.args("to")
+    print(currency2);
+    amount = request.args.get("amount")
+    print(amount)
+
+    return render_template("form.html")
